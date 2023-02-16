5accc28788a706fca116fd9d1dbd20aa7edb9c70
Behnur Yildiz <behnuryildiz@hotmail.com>
2023-02-16 21:06:48 +0100

    new files created, escpecially for different containers there are different files now. table.sh extension to a sql extention changed, Dockerfiles for both amended

---
+++ mysql/table.sql
@@ -0,0 +1,20 @@
+#! /bin/bash
+CREATE DATABASE IF NOT EXISTS customer_data;
+USE customer_data;
+
+CREATE TABLE owners (
+    owner_id int NOT NULL AUTO_INCREMENT,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    PRIMARY KEY (owner_id)
+); 
+
+CREATE TABLE pets (
+    pet_id int NOT NULL AUTO_INCREMENT,
+    fk_owned_by int,
+    pet_name varchar(255) NOT NULL,
+    species varchar(255) NOT NULL,
+    PRIMARY KEY (pet_id),
+    FOREIGN KEY (fk_owned_by) REFERENCES Owners(owner_id)
+); 
